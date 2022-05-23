0e04be7e4b745bedd3bbb1eccb1fc103a8dacc2e
FraAccaBi <francescahuda@gmail.com>
2022-05-23 17:22:56 +0200

    backup commit

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $nav = [
     'Tecnologie',
     'Domande frequenti'
 ];
-
+$logo_img = "https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg"
 ?>
 
 <!DOCTYPE html>
@@ -70,11 +70,11 @@ $nav = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<header>
+<header class="d-flex">
     <div class="logo">
-
+        <img src="<?= $logo_img; ?>" alt="">
     </div>
-    <h1>Privacy & Termini</h1>
+    <h1 class="h5">Privacy & Termini</h1>
 </header>
 <nav>
     <ul class="d-flex">
@@ -83,20 +83,23 @@ $nav = [
         <?php endforeach ?>
     </ul>
 </nav>
-    
-<?php foreach($faqs as $element): ?>
 
-    <h2> <?= $element['domanda']; ?> </h2>
+<main class="container">
+    <?php foreach($faqs as $element): ?>
 
-    <?php foreach($element as $risposte): ?>
+        <h2> <?= $element['domanda']; ?> </h2>
 
-        <?php foreach($risposte as $key => $risposta): ?>
+        <?php foreach($element as $risposte): ?>
 
-            <p> <?= $risposta; ?> </p>
-            
+            <?php foreach($risposte as $key => $risposta): ?>
+
+                <p> <?= $risposta; ?> </p>
+                
+            <?php endforeach ?>
         <?php endforeach ?>
     <?php endforeach ?>
-<?php endforeach ?>
+</main>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
